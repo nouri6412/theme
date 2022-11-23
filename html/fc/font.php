80827e755d9c5888f0f80734e53df4d6addab20f
Jalil Nouri <nouri.6412@gmail.com>
2022-11-23 16:16:18 +0330

    gggg

---
+++ html/fc/font.php
@@ -0,0 +1,2 @@
+<?php
+ file_put_contents('test/test.css', fopen('https://themes.pixelstrap.com/fastkart/assets/css/vendors/bootstrap.css', 'r'));
--- html/fc/index.php
+++ html/fc/index.php
@@ -1,39 +1,91 @@
 <?php
+set_time_limit(6000);
 $base_url = 'https://themes.pixelstrap.com/fastkart/';
-//file_put_contents("test.css", fopen("https://themes.pixelstrap.com/fastkart/assets/css/vendors/bootstrap.css", 'r'));
 
-$file = @file_get_contents('http://localhost:84/theme/html/fc/index.html');
+$site_url = 'https://themes.pixelstrap.com/fastkart/front-end/index.html';
 
-$urls = [];
+$file_name = 'index.html';
 $pattern = "/\"https:\/\/themes.pixelstrap.com\/fastkart\/[^\"]+\"/"; //  pattern=> \"XXX[^\"]+\"
-//$pattern="/assets/";
-preg_match_all($pattern, $file, $urls);
 
-//var_dump($urls);
-$path = "";
-foreach ($urls[0] as $url) {
+function fetch($_base_url, $_site_url, $_file_name, $_pattern, $out_path = '../', $extra_path = '')
+{
+    $base_url = $_base_url;
+    $site_url = $_site_url;
+    $file_name = $extra_path . $_file_name;
+    $pattern = $_pattern;
+    //echo $_site_url . '<br>';
+    // echo dirname(__FILE__)  . '<br>';
+    file_put_contents($file_name, fopen($site_url, 'r'));
+
+
+    $file = @file_get_contents($file_name);
+
+
+    if (strlen($extra_path) > 0) {
+     //  echo $out_path.'----'. $base_url .'----'. $extra_path;
+    }
+
+    $file = str_replace($out_path, $base_url . $extra_path, $file);
+
+    file_put_contents($file_name, $file);
+
+
+
+
+    $urls = [];
+
+    //$pattern="/assets/";
+    preg_match_all($pattern, $file, $urls);
+    if (strlen($extra_path) > 0) {
+       // var_dump($urls);
+        //return;
+    }
+
     $path = "";
-    $base_file = str_replace('"', '', $url);
-    $temp_url = str_replace($base_url, '', $url);
-    $temp_url = str_replace('"', '', $temp_url);
+    foreach ($urls[0] as $url) {
+        $path = "";
+        $base_file = str_replace('"', '', $url);
+        $temp_url = str_replace($base_url, '', $url);
+        $temp_url = str_replace('"', '', $temp_url);
 
-    $ex = explode("/", $temp_url);
+        $ex = explode("/", $extra_path . $temp_url);
 
-    $vir = "";
-    for ($x = 0; $x < count($ex) - 1; $x++) {
-        $path .= $vir . $ex[$x];
-        $vir = "/";
-        if (!file_exists(stream_resolve_include_path($path))) {
-            echo $path . '<br>';
-            mkdir($path, 0777, true);
+        $vir = "";
+        for ($x = 0; $x < count($ex) - 1; $x++) {
+            $path .= $vir . $ex[$x];
+            $vir = "/";
+            if (!file_exists(stream_resolve_include_path($path))) {
+                if (strlen($extra_path) > 0) {
+                    //  echo $path . '<br>';
+                } else {
+                    mkdir($path, 0777, true);
+                }
+            }
+        }
+
+        $fff =preg_split('/ (?|#) /', $ex[count($ex) - 1]);
+
+        $ex_pt = str_replace($base_url, '', $path);
+        $path .= '/' . $fff[0];
+
+        if (strlen($fff[0]) > 4 && substr($fff[0], -4) == '.css') {
+            // echo $ex_pt . '<br>';
+            fetch($base_url, $base_url . $path, $fff[0], $pattern, '../', $ex_pt . '/');
+        } else {
+            if (!file_exists(stream_resolve_include_path($path))) {
+                file_put_contents($path, fopen($base_file, 'r'));
+            }
         }
     }
-    $path .= '/' . $ex[count($ex) - 1];
-    if (!file_exists(stream_resolve_include_path($path))) {
-        file_put_contents($path, fopen($base_file, 'r'));
-    }
-}
 
+    $file = @file_get_contents($file_name);
+
+
+    $file = str_replace($base_url, '', $file);
+
+    file_put_contents($file_name, $file);
+}
+fetch($base_url, $site_url, $file_name, $pattern);
 // if (!file_exists(stream_resolve_include_path('new-folder'))) {
 //     mkdir('new-folder', 0777, true);
 // }
