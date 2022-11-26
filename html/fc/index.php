4c08ccdf95f39e7960b7320f04d5faa5ad714cc4
Jalil Nouri <nouri.6412@gmail.com>
2022-11-26 12:31:09 +0330

    gggg

--- html/fc/index.php
+++ html/fc/index.php
@@ -1,17 +1,26 @@
 <?php
 set_time_limit(6000);
-$links=[];
+$links = [];
 
 $base_url = 'https://themes.pixelstrap.com/fastkart/';
 
-$site_url = 'https://themes.pixelstrap.com/fastkart/front-end/index.html';
-
 $file_name = 'index.html';
+$root = 'https://themes.pixelstrap.com/fastkart/front-end/';
+$site_url = $root . $file_name;
+$index_fetch = 1;
+
+
+
+
+
+$links[$file_name] = $file_name;
+
 $pattern = "/\"https:\/\/themes.pixelstrap.com\/fastkart\/[^\"]+\"/"; //  pattern=> \"XXX[^\"]+\"
 
 function fetch($_base_url, $_site_url, $_file_name, $_pattern, $out_path = '../', $extra_path = '')
 {
-    global $links;
+    echo '<div>' . $_base_url . '---' . $_site_url . '...' . $_file_name . '...' . $_pattern . '</div>';
+    global $links, $root, $index_fetch;
     $base_url = $_base_url;
     $site_url = $_site_url;
     $file_name = $extra_path . $_file_name;
@@ -22,6 +31,8 @@ function fetch($_base_url, $_site_url, $_file_name, $_pattern, $out_path = '../'
 
 
     $file = @file_get_contents($file_name);
+
+
 
 
     if (strlen($extra_path) > 0) {
@@ -39,13 +50,15 @@ function fetch($_base_url, $_site_url, $_file_name, $_pattern, $out_path = '../'
 
     preg_match_all($pattern, $file, $urls);
     if (strlen($extra_path) > 0) {
-     //   var_dump($urls);
+        //   var_dump($urls);
         //  return;
     }
 
     $path = "";
+    $arr_path = [];
     foreach ($urls[0] as $url) {
         $path = "";
+        $arr_path = [];
         $base_file = str_replace('"', '', $url);
         $temp_url = str_replace($base_url, '', $url);
         $temp_url = str_replace('"', '', $temp_url);
@@ -55,39 +68,88 @@ function fetch($_base_url, $_site_url, $_file_name, $_pattern, $out_path = '../'
         $vir = "";
         for ($x = 0; $x < count($ex) - 1; $x++) {
             $path .= $vir . $ex[$x];
+            $arr_path[] = $path;
             $vir = "/";
             if (!file_exists(stream_resolve_include_path($path))) {
                 mkdir($path, 0777, true);
             }
         }
 
-        $fff = preg_split('/ (?|#) /', $ex[count($ex) - 1]);
+        $fff = preg_split('/(?|#)/', $ex[count($ex) - 1]);
 
         $ex_pt = str_replace($base_url, '', $path);
         $path .= '/' . $fff[0];
 
         if (strlen($fff[0]) > 5 && substr($fff[0], -5) == '.html') {
             // echo $ex_pt . '<br>';
-           // fetch($base_url, $base_url . $path, $fff[0], $pattern, '../', $ex_pt . '/');
+            // fetch($base_url, $base_url . $path, $fff[0], $pattern, '../', $ex_pt . '/');
 
-        }
-      else  if (strlen($fff[0]) > 4 && substr($fff[0], -4) == '.css') {
+        } else  if (strlen($fff[0]) > 4 && substr($fff[0], -4) == '.css') {
             // echo $ex_pt . '<br>';
-           // fetch($base_url, $base_url . $path, $fff[0], $pattern, '../', $ex_pt . '/');
+            // fetch($base_url, $base_url . $path, $fff[0], $pattern, '../', $ex_pt . '/');
 
             if (!file_exists(stream_resolve_include_path($path))) {
                 file_put_contents($path, fopen($base_file, 'r'));
             }
             $sub_file = @file_get_contents($path);
-            $sub_urls=[];
-            $sub_pattern = "/url\(\"[^\"]+(\.eot|\.woff|\.ttf|\.png|\.jpg|\.jpeg|\.gif|\.svg)+\"\)|url\(\"[^\"]+(\.eot|\.woff|\.ttf|\.png|\.jpg|\.jpeg|\.gif|\.svg)+[^\"]+\"\)/";
-          echo $path;
-            preg_match_all($sub_pattern, $sub_file, $sub_urls);
-           var_dump($sub_urls);
+            $sub_urls = [];
+            $sub_pattern = "/(url\([^\)]+\))|url\(\"[^\"]+(\.eot|\.woff|\.ttf|\.png|\.jpg|\.jpeg|\.gif|\.svg)+\"\)|url\(\"[^\"]+(\.eot|\.woff|\.ttf|\.png|\.jpg|\.jpeg|\.gif|\.svg)+[^\"]+\"\)|url\(\'[^\']+(\.eot|\.woff|\.ttf|\.png|\.jpg|\.jpeg|\.gif|\.svg)+\'\)|url\(\'[^\']+(\.eot|\.woff|\.ttf|\.png|\.jpg|\.jpeg|\.gif|\.svg)+[^\']+\'\)/";
 
+            preg_match_all($sub_pattern, $sub_file, $sub_urls);
+
+            if (isset($sub_urls[0]) && is_array($sub_urls[0])) {
+                $path_1 = "";
+                foreach ($sub_urls[0] as $item) {
+                    $path_1 = "";
+
+                    $temp_url_1 = str_replace($base_url, '', $item);
+                    $temp_url_1 = str_replace('"', '', $temp_url_1);
+                    $temp_url_1 = str_replace('\'', '', $temp_url_1);
+                    $temp_url_1 = str_replace('url(', '', $temp_url_1);
+                    $temp_url_1 = str_replace(')', '', $temp_url_1);
+
+
+                    $pe = explode('../', $temp_url_1);
+
+                    $vir_1 = "";
+                    if (isset($arr_path[count($arr_path) - count($pe)])) {
+                        $vir_1 = "/";
+                        $path_1 = $arr_path[count($arr_path) - count($pe)];
+                    }
+
+
+                    $temp_url_1 = str_replace('../', '', $temp_url_1);
+                    $temp_url_1 = str_replace('./', '', $temp_url_1);
+
+
+                    $ex_1 = explode("/", $extra_path . $temp_url_1);
+
+                    if (count($ex_1) > 1) {
+                        for ($x_1 = 0; $x_1 < count($ex_1) - 1; $x_1++) {
+                            $path_1 .= $vir_1 . $ex_1[$x_1];
+                            $vir_1 = "/";
+                            if (!file_exists(stream_resolve_include_path($path_1))) {
+
+                                mkdir($path_1, 0777, true);
+                            }
+                        }
+                    }
+
+
+                    $fff_1 = preg_split('/(\?|\#|\?\#)/', $ex_1[count($ex_1) - 1]);
+                    // echo '<div>' . $ex_1[count($ex_1) - 1] . '</div>';
+
+                    $ex_pt = str_replace($base_url, '', $path_1);
+                    $path_1 .= '/' . $fff_1[0];
+
+                    if (!file_exists(stream_resolve_include_path($path_1))) {
+                        file_put_contents($path_1, fopen($base_url . '/' . $path_1, 'r'));
+                    }
+                }
+            }
         } else {
             if (!file_exists(stream_resolve_include_path($path))) {
-               // file_put_contents($path, fopen($base_file, 'r'));
+                file_put_contents($path, fopen($base_file, 'r'));
             }
         }
     }
@@ -99,9 +161,35 @@ function fetch($_base_url, $_site_url, $_file_name, $_pattern, $out_path = '../'
         $file = str_replace($base_url, '', $file);
 
         file_put_contents($file_name, $file);
+
+        $pattern_1 = "/href=\"[^\"]+(\.html)+\"/";
+        $ur = [];
+        preg_match_all($pattern_1, $file, $ur);
+
+        // $index_fetch++;
+        // if($index_fetch > 2)
+        // {
+        //    // return;
+        // }
+        foreach ($ur[0] as $it) {
+            $it_url = str_replace('href="', '', $it);
+            $it_url = str_replace('"', '', $it_url);
+            if (!isset($links[$it_url])) {
+                $links[$it_url] = $it_url;
+                $temp_url_1 = $it_url;
+                $url_arr_1 = explode('/', $temp_url_1);
+                if (count($url_arr_1) > 1) {
+                    fetch($base_url, $base_url . $it_url, $it_url, $pattern);
+                } else {
+                    fetch($base_url, $root . $it_url, $it_url, $pattern);
+                }
+            }
+        }
     }
 }
+
 fetch($base_url, $site_url, $file_name, $pattern);
+//var_dump($links);
 // if (!file_exists(stream_resolve_include_path('new-folder'))) {
 //     mkdir('new-folder', 0777, true);
 // }
